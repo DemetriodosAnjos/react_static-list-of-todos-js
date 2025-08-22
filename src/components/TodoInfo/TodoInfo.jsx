8b79dba519a4c8f15acc3913566b80d4af77c262
Demetrio dos Anjos <ddoanjos@gmail.com>
2025-08-22 16:49:44 -0300

    fix: Renderiza UserInfo condicionalmente em TodoInfo

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,15 +1,14 @@
 // src/components/TodoInfo/TodoInfo.jsx
 import React from 'react';
-import { UserInfo } from '../UserInfo/UserInfo';
+import { UserInfo } from '../UserInfo/UserInfo'; // Importe o componente UserInfo
 
 export const TodoInfo = ({ todo }) => {
-  // Lógica para adicionar a classe "TodoInfo--completed" se o todo estiver completo
   const todoClasses = `TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`;
 
   return (
     <article className={todoClasses}>
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
