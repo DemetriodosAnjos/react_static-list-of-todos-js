208cfb765b86d9e923760f5ba704b6bac73bbe7f
Demetrio dos Anjos <ddoanjos@gmail.com>
2025-08-22 16:40:27 -0300

    feat: Refatora a lista de tarefas para componentes modulares

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+// src/components/TodoInfo/TodoInfo.jsx
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  // Lógica para adicionar a classe "TodoInfo--completed" se o todo estiver completo
+  const todoClasses = `TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`;
+
+  return (
+    <article className={todoClasses}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
